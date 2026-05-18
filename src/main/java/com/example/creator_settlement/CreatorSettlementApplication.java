4eb2d15d0116a0ca4f692c0f3a300a31672e2127
kimsojin <100557009+rlathwls03@users.noreply.github.com>
2026-05-18 16:27:07 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/example/creator_settlement/CreatorSettlementApplication.java
@@ -0,0 +1,13 @@
+package com.example.creator_settlement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorSettlementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatorSettlementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/creator_settlement/CreatorSettlementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creator_settlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorSettlementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
